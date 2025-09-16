145616f4a3b3fbcf5056dcbb84129e0fd688fd03
P4R1H <damps.home@gmail.com>
2025-09-16 19:22:28 +0530

    basic setup complete

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"GoSocial/internal/db"
 	"GoSocial/internal/env"
 	"GoSocial/internal/store"
 	"log"
@@ -13,13 +14,20 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres://user:password@localhost/dbname?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:password@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15mins"),
 		},
 	}
-	store := store.NewStorage(nil)
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Printf("Database connection pool established.")
+
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
