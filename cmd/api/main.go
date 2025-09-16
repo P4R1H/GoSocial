ab43dba368aac2eea43576ec3013c526180156b3
P4R1H <damps.home@gmail.com>
2025-09-16 18:42:11 +0530

    db connection establishment

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"GoSocial/internal/env"
+	"GoSocial/internal/store"
 	"log"
 
 	"github.com/joho/godotenv"
@@ -11,11 +12,18 @@ func main() {
 	godotenv.Load()
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:password@localhost/dbname?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15mins"),
+		},
 	}
+	store := store.NewStorage(nil)
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
-
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
