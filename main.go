17ccd81c553fb3c5578134931fd57bb2153cbcad
P4R1H <damps.home@gmail.com>
2025-09-16 14:09:47 +0530

    Basic API setup

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	api := &api{addr: ":8080"}
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    api.addr,
+		Handler: mux,
+	}
+
+	mux.HandleFunc("GET /users", api.getUsersHandler)
+
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
